cad98ce2ff449fe5083a5d3f4e82135f53e6aaff
Lucas Batista de Carvalho <lucas.carvalho@xpi.com.br>
2021-01-31 20:00:04 -0300

    Envio de mensagens para o teams

--- XP.Hackathon.Zabbot.Interface/Service/IZabbixService.cs
+++ XP.Hackathon.Zabbot.Interface/Service/IZabbixService.cs
@@ -5,6 +5,6 @@ namespace XP.Hackathon.Zabbot.Interface.Service
 {
     public interface IZabbixService
     {
-        Task<ResultBase> ReceiveMessage(ZabbixEvent message);
+        Task<ResultBase> SendMessageToTeams(ZabbixEvent message);
     }
 }
---
+++ XP.Hackathon.Zabbot.Model/Configuration/Settings.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace XP.Hackathon.Zabbot.Model.Configuration
+{
+    public class Settings : ISettings
+    {
+        public string TeamsApi { get; set; }
+        public TokenConfiguration TokenConfiguration { get; set; }
+    }
+}
