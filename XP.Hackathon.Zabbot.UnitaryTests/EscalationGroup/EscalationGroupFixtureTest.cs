4599bb27c484a1b06288785ac6d1b403522a4425
Lucas Batista de Carvalho <lucas.carvalho@xpi.com.br>
2021-01-31 11:39:04 -0300

    Testes unitarios

---
+++ XP.Hackathon.Zabbot.UnitaryTests/EscalationGroup/EscalationGroupFixtureTest.cs
@@ -0,0 +1,40 @@
+﻿using AutoMoq;
+using Moq;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using XP.Hackathon.Zabbot.Interface.Repository;
+using XP.Hackathon.Zabbot.Interface.Service;
+using XP.Hackathon.Zabbot.Model;
+using XP.Hackathon.Zabbot.Service;
+
+namespace XP.Hackathon.Zabbot.Tests
+{
+    public class EscalationGroupFixtureTest
+    {
+        public Mock<IEscalationGroupRepository> _escalationGroupRepository;
+        public Mock<ILogService> _logService;  
+
+        public EscalationGroupService GetEscalationGroupMoq()
+        {
+            var mocker = new AutoMoqer();
+            var _escalationGroupService = mocker.Resolve<EscalationGroupService>();
+
+
+
+            _escalationGroupRepository = mocker.GetMock<IEscalationGroupRepository>();
+            _logService = mocker.GetMock<ILogService>();
+
+            _escalationGroupRepository.Setup(s => s.GetAsync()).Returns(GetAllEscalationGroup());
+
+            return _escalationGroupService;
+        }
+
+        public Task<Result<EscalationGroup>> GetAllEscalationGroup()
+        {
+            var json = "[{\"Name\":\"Squad Assessoria Web\",\"Email\":\"squad-assessoria-web@xpi.com.br\",\"TagName\":\"SquadAssessoriaWeb\",\"Channel\":\"webhook-xxxxxxxxxxxxxxxxxxxxxxx\",\"Id\":1,\"Active\":true,\"Created\":\"2021-01-30T12:22:21.04\",\"Updated\":\"2021-01-30T15:21:01.41\"},{\"Name\":\"Squad Assessoria Comissões\",\"Email\":\"squad-assessoria-comissoes@xpi.com.br\",\"TagName\":\"SquadAssessoriaComissoes\",\"Channel\":\"canal do teams\",\"Id\":2,\"Active\":true,\"Created\":\"2021-01-30T17:38:38.337\",\"Updated\":null}]";
+            var customers = JsonConvert.DeserializeObject<Task<Result<EscalationGroup>>>(json);
+
+            return customers;
+        }
+    }
+}
---
+++ XP.Hackathon.Zabbot.UnitaryTests/EscalationGroup/EscalationGroupServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace XP.Hackathon.Zabbot.Tests
+{
+    [TestClass]
+    public class EscalationGroupServiceTest
+    {
+        public EscalationGroupFixtureTest _fixture { get; set; }
+
+        public EscalationGroupServiceTest()
+        {
+            _fixture = new EscalationGroupFixtureTest();
+        }
+
+        [TestMethod("Obtém as lista de times")]
+        public async void EscalationGroup_GetAll_Success()
+        {
+            // Arrange
+            var escalationGroupList = _fixture.GetAllEscalationGroup();
+            var _service = _fixture.GetEscalationGroupMoq();
+
+            _fixture._escalationGroupRepository.Setup(s => s.GetAsync()).Returns(escalationGroupList);
+
+            // Act
+            var result = await _service.GetAsync();
+
+            // Assert
+            _fixture._escalationGroupRepository.Verify(r => r.GetAsync(), Times.Once);
+            result.Should().NotBeNull();
+        }
+    }
+}
